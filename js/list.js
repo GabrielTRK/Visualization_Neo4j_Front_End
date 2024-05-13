e9b86b0059cf7bb135a2149a0560cd0051d8c47a
GabrielTRK <g6323p@gmail.com>
2024-05-13 18:41:52 +0100

    Added switches to Load Project page and added plus/minus signs

--- js/list.js
+++ js/list.js
@@ -1,3 +1,5 @@
+switchsIDs = ['EU', 'NA', 'SA', 'AS', 'AF']
+switchsNames = ['Europe', 'North America', 'South America', 'Asia', 'Africa']
 
 url = 'http://localhost:8080/loadP'
 
@@ -31,9 +33,32 @@ fetch(url).then(res => {
 
             addEpi(divEpi, dataBack[i])
 
+            polP = document.createElement("p")
+            polP.innerHTML = 'Political constraint:'
+
+            divSwitchs = document.createElement("div")
+            divSwitchs.style.display = 'flex'
+            divSwitchs.style.justifyContent = 'space-around'
+
+            addSwitchs(divSwitchs, dataBack[i])
+
+            decP = document.createElement("p")
+            decP.innerHTML = 'Decision maker\'s preferences:'
+
+            divDecMain = document.createElement('div')
+            divDecMain.classList.add('parent')
+            divDecMain.classList.add('mb-3')
+
+            addPrefImgs(divDecMain, dataBack)
+            
             projectI.appendChild(divInputs)
             projectI.appendChild(epiP)
             projectI.appendChild(divEpi)
+            projectI.appendChild(polP)
+            projectI.appendChild(divSwitchs)
+            projectI.appendChild(decP)
+            projectI.appendChild(divDecMain)
+
             main.appendChild(projectI)
         }
     }
@@ -104,4 +129,52 @@ function addEpi(divEpi, dataBack){
     divProgress.style.width = String(dataBack.res.epi) + '%'
     divProgress.innerHTML = String(dataBack.res.epi) + '%'
     divEpi.appendChild(divProgress)
+}
+
+function addSwitchs(divSwitchs, dataBack){
+    for(j = 0; j < switchsIDs.length; j++){
+        divSwitchI = document.createElement('div')
+        divSwitchI.classList.add('form-check')
+        divSwitchI.classList.add('form-switch')
+
+        inputSwitchI = document.createElement('input')
+        inputSwitchI.classList.add('form-check-input')
+        inputSwitchI.setAttribute('type', 'checkbox')
+        inputSwitchI.setAttribute('disabled', 'true')
+        inputSwitchI.setAttribute('id', switchsIDs[j]+dataBack.nombre)
+        inputSwitchI.checked = dataBack.res.pol.includes(switchsIDs[j])
+
+        labelSwitchI = document.createElement('label')
+        labelSwitchI.classList.add('form-check-label')
+        labelSwitchI.setAttribute('for', switchsIDs[j]+dataBack.nombre)
+        labelSwitchI.innerHTML = switchsNames[j]
+
+        divSwitchI.appendChild(inputSwitchI)
+        divSwitchI.appendChild(labelSwitchI)
+        divSwitchs.appendChild(divSwitchI)
+    }
+    
+}
+
+function addPrefImgs(divDecMain, dataBack){
+
+    divChildPlusIMG = document.createElement('div')
+    divChildPlusIMG.classList.add('child')
+    divPlusImg = document.createElement('div')
+    divPlusImg.style.display = 'flex'
+    divPlusImg.style.justifyContent = 'center'
+    divPlusImg.innerHTML = '<svg  width="40%" fill="currentColor"class="bi bi-file-plus" viewBox="0 0 16 16"><path d="M8.5 6a.5.5 0 0 0-1 0v1.5H6a.5.5 0 0 0 0 1h1.5V10a.5.5 0 0 0 1 0V8.5H10a.5.5 0 0 0 0-1H8.5z"></path><path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1" /></svg>'
+
+    divChildMinusIMG = document.createElement('div')
+    divChildMinusIMG.classList.add('child')
+    divMinusImg = document.createElement('div')
+    divMinusImg.style.display = 'flex'
+    divMinusImg.style.justifyContent = 'center'
+    divMinusImg.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="40%" fill="currentColor"class="bi bi-file-minus" viewBox="0 0 16 16"><path d="M5.5 8a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1-.5-.5" /><path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1" /></svg>'
+    
+    divChildPlusIMG.appendChild(divPlusImg)
+    divChildMinusIMG.appendChild(divMinusImg)
+    divDecMain.appendChild(divChildPlusIMG)
+    divDecMain.appendChild(divChildMinusIMG)
+
 }
