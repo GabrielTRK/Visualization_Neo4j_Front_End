172da11deac8cd154e66d5e0f938dc557ccaae88
GabrielTRK <g6323p@gmail.com>
2024-05-22 20:22:21 +0100

    Added dynamic creation and filtering of table in details.html

--- js/details.js
+++ js/details.js
@@ -5,9 +5,9 @@ currentDay = 0
 currentAir = ''
 separator = '/'
 minDate = new Date("2020/01/01")
+maxDate = new Date("2020/01/01")
 minDateString = ''
-
-var params = { numDias: 0, diaActual: 0 };
+numDias = 0
 
 mainURL = 'http://localhost:8080/' + projectName + separator + solutionID + separator
 
@@ -18,15 +18,51 @@ function getData() {
         return res.json()
     })
         .then(dataBack => {
-            //main = document.getElementById("MainList")
             //Si databack.length == 0 poner mensaje de empty list
             console.log(dataBack)
             document.getElementById('Sdate').value = dataBack.fechas.Fecha_Actual
             document.getElementById('Sdate').min = dataBack.fechas.Fecha_Inicial
             document.getElementById('Sdate').max = dataBack.fechas.Fecha_Final
             minDate = new Date(dataBack.fechas.Fecha_Inicial)
+            maxDate = new Date(dataBack.fechas.Fecha_Final)
+            numDias = Math.round((maxDate.getTime() - minDate.getTime()) / (1000 * 3600 * 24))
             minDateString = dataBack.fechas.Fecha_Inicial
-            
+
+            row1 = document.getElementById('row1')
+            row2 = document.getElementById('row2')
+
+            th1 = document.createElement('th')
+            th1.setAttribute("scope", "col");
+            th1.classList.add('text-center')
+            th1.innerHTML = 'Connections'
+
+            th2 = document.createElement('th')
+            th2.setAttribute("scope", "col");
+            th2.classList.add('text-center')
+            th2.innerHTML = 'Bits'
+
+            row1.appendChild(th1)
+            row2.appendChild(th2)
+            for(i = 0; i < dataBack.coordenadasConexiones.length; i++){
+                row1TH = document.createElement('th')
+                row1TH.setAttribute("scope", "col");
+                row1TH.classList.add('text-center')
+                row1TH.innerHTML = dataBack.coordenadasConexiones[i].iataOrigen + ' - ' + dataBack.coordenadasConexiones[i].iataDestino
+
+                row2TD = document.createElement('td')
+                row2TD.setAttribute("scope", "col");
+                row2TD.classList.add('text-center')
+                if(dataBack.coordenadasConexiones[i].abierto_cerrado){
+                    row2TD.innerHTML = '1'
+                }else{
+                    row2TD.innerHTML = '0'
+                }
+
+                row1.appendChild(row1TH)
+                row2.appendChild(row2TD)
+            }
+            document.getElementById('tableCaption').innerHTML = dataBack.coordenadasConexiones.length + ' connections'
+
         }
         )
 }
@@ -34,17 +70,17 @@ function getData() {
 function datesDifference(fecha) {
     let date1 = new Date(fecha);
     currentDay = Math.round((date1.getTime() - minDate.getTime()) / (1000 * 3600 * 24))
-    if (currentDay < 0) {
+    if (currentDay < 0 || currentDay > numDias) {
         currentDay = 0
         document.getElementById('Sdate').value = minDateString
     }
 }
 
 function applyFilter() {
-    document.getElementById('row3').remove()
     currentAir = document.getElementById('airInput').value
     datesDifference(document.getElementById('Sdate').value)
-    //Quitar children de row1 y row2
+    removeChildren(document.getElementById('row1'))
+    removeChildren(document.getElementById('row2'))
     getData()
 }
 
@@ -53,7 +89,8 @@ function resetFilter() {
     currentDay = 0
     document.getElementById('Sdate').value = minDateString
     document.getElementById('airInput').value = ''
-    //Quitar children de row1 y row2
+    removeChildren(document.getElementById('row1'))
+    removeChildren(document.getElementById('row2'))
     getData()
 }
 
